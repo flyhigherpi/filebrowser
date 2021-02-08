17b7ba399a636db4b9337bea8c702ba82508e6db
sadoneli <sadoneli@gmail.com>
2021-02-08 15:57:03 +0800

    repack

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "FileBrowser：您的可视化路由文件管理系统", 
     "home_url": "Module_filebrowser.asp", 
-    "md5": "21d0c99ed49123988b2355b66adbaf01", 
+    "md5": "c8b3276460f9e227fe9781876d38b80c", 
     "module": "filebrowser", 
     "title": "FileBrowser", 
     "version": "1.0.6"
