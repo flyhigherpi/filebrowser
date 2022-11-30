47f0d2945662d417a202ae659b9b82069a8d62f4
sadog <sadoneli@gmail.com>
2022-11-30 23:45:51 +0800

    update config.json.js

--- config.json.js
+++ config.json.js
@@ -1,8 +1,11 @@
 {
-    "description": "FileBrowser：您的可视化路由文件管理系统", 
-    "home_url": "Module_filebrowser.asp", 
-    "md5": "916535afdf85dbb875c9b09c710044b1", 
-    "module": "filebrowser", 
-    "title": "FileBrowser", 
-    "version": "2.0.0"
+	"description": "FileBrowser：您的可视化路由文件管理系统", 
+	"home_url": "Module_filebrowser.asp", 
+	"md5": "916535afdf85dbb875c9b09c710044b1", 
+	"module": "filebrowser", 
+	"title": "FileBrowser", 
+	"tags":"USB",
+	"author":"MAN KAI, sadog",
+	"link":"https://github.com/flyhigherpi/filebrowser",
+	"version": "2.0.0"
 }
