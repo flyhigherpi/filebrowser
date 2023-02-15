aafe3822c0d8f93cd60a42cd43f7db3564cb1345
wengheng <wengheng@everstu.com>
2023-02-15 16:29:30 +0800

    fix:build tar.gz

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "description": "FileBrowser：您的可视化路由文件管理系统", 
     "home_url": "Module_filebrowser.asp", 
     "link": "https://github.com/flyhigherpi/filebrowser", 
-    "md5": "99fcb6ecf604c71481a0df00fbd984eb", 
+    "md5": "67fb18846af597621961163a21de4120", 
     "module": "filebrowser", 
     "tags": "USB", 
     "title": "FileBrowser", 
