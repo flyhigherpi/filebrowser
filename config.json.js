310ce3bf45575ba86ac55bb3b70f5d2a01d1ee05
wengheng <wengheng@everstu.com>
2023-02-15 15:08:59 +0800

    update:Optimize startup process,can run in /koolshare

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "FileBrowser：您的可视化路由文件管理系统", 
     "home_url": "Module_filebrowser.asp", 
     "link": "https://github.com/flyhigherpi/filebrowser", 
-    "md5": "f0e1bd7ae51f6d0b8c050449791e299a", 
+    "md5": "99fcb6ecf604c71481a0df00fbd984eb", 
     "module": "filebrowser", 
     "tags": "USB", 
     "title": "FileBrowser", 
-    "version": "2.0.2"
+    "version": "2.0.3"
 }
